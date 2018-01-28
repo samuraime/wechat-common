7822ec0becbb8644e92da57e6349fe8d2e308ba5
SamuraiMe <samurai7@foxmail.com>
2018-01-28 09:45:12 -0500

    update test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,10 @@ const { expect } = require('chai');
 const Wechat = require('../');
 const { appid, secret, url } = require('./wechat.config');
 
+const delay = time => new Promise((resolve) => {
+  setTimeout(resolve, time);
+});
+
 describe('Wechat', () => {
   describe('Wechat.getAccessToken', () => {
     it('should be ok', async () => {
@@ -44,9 +48,8 @@ describe('Wechat', () => {
     it('should be ok', async () => {
       const wechat = new Wechat(appid, secret);
       wechat.getAccessToken()
-        .then((res) => {
-          expect(res.accessToken).to.be.a('string');
-          expect(res.expiresIn).to.be.a('number');
+        .then((accessToken) => {
+          expect(accessToken).to.be.a('string');
         })
         .catch((e) => {
           expect(e.message).to.include('WechatAPI');
@@ -57,9 +60,8 @@ describe('Wechat', () => {
     it('should be ok', async () => {
       const wechat = new Wechat(appid, secret);
       wechat.getJSAPITicket()
-        .then((res) => {
-          expect(res.ticket).to.be.a('string');
-          expect(res.expiresIn).to.be.a('number');
+        .then((ticket) => {
+          expect(ticket).to.be.a('string');
         })
         .catch((e) => {
           expect(e.message).to.include('WechatAPI');
@@ -81,4 +83,23 @@ describe('Wechat', () => {
         });
     });
   });
+  describe('cached accessToken', () => {
+    it('should be get same token', async () => {
+      const wechat = new Wechat(appid, secret);
+      const accessToken = await wechat.getAccessToken();
+      await delay(1000);
+      const accessToken2 = await wechat.getAccessToken();
+      expect(accessToken).to.equal(accessToken2);
+    });
+  });
+  describe('cached ticket', () => {
+    it('should be get same ticket', async () => {
+      const wechat = new Wechat(appid, secret);
+      const ticket = await wechat.getJSAPITicket();
+      await delay(1000);
+      const ticket2 = await wechat.getJSAPITicket();
+      expect(ticket).to.equal(ticket2);
+    });
+  });
 });
+
